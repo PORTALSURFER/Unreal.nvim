82cfc46652224c6827cbbe1c213c0d188c8e38c4
PORTALSURFER <portalsurfer@gmail.com>
2023-11-05 14:13:55 +0100

    restarting code

---
+++ plugin/init.lua
@@ -0,0 +1,10 @@
+local init = {}
+
+function init.checks()
+	if 1 ~= vim.fn.has("nvim-0.10.0") then
+		vim.api.nvim_err_writeln("Unreal.nvim requires at least nvim-0.7.0")
+		return
+	end
+end
+
+return init
--- plugin/unreal.lua
+++ plugin/unreal.lua
@@ -1,44 +1,43 @@
-if 1 ~= vim.fn.has "nvim-0.7.0" then
-  vim.api.nvim_err_writeln "Unreal.nvim requires at least nvim-0.7.0"
-  return
-end
+init = require("init")
 
-if vim.g.loaded_unrealnvim == 1 then
-  return
-end
-vim.g.loaded_unrealnvim = 1
+init.checks()
 
--- [[Link Commands]]
-vim.api.nvim_create_user_command("UnrealGen", function(opts)
-    require("unreal.commands").generateCommands(opts)
-end, {
-})
+-- if vim.g.loaded_unrealnvim == 1 then
+--   return
+-- end
+-- vim.g.loaded_unrealnvim = 1
 
-vim.api.nvim_create_user_command("UnrealGenWithEngine", function(opts)
-    if not opts then
-        opts = {}
-    end
+-- -- [[Link Commands]]
+-- vim.api.nvim_create_user_command("UnrealGen", function(opts)
+--     require("unreal.commands").generateCommands(opts)
+-- end, {
+-- })
 
-    opts.WithEngine = true
-    require("unreal.commands").generateCommands(opts)
-end, {
-})
+-- vim.api.nvim_create_user_command("UnrealGenWithEngine", function(opts)
+--     if not opts then
+--         opts = {}
+--     end
 
-vim.api.nvim_create_user_command("UnrealBuild", function(opts)
-    require("unreal.commands").build(opts)
-end, {
-})
+--     opts.WithEngine = true
+--     require("unreal.commands").generateCommands(opts)
+-- end, {
+-- })
 
-vim.api.nvim_create_user_command("UnrealRun", function(opts)
-    require("unreal.commands").run(opts)
-end, {
-})
+-- vim.api.nvim_create_user_command("UnrealBuild", function(opts)
+--     require("unreal.commands").build(opts)
+-- end, {
+-- })
 
-vim.api.nvim_create_user_command("UnrealCD", function(opts)
-    require("unreal.commands").SetUnrealCD(opts)
-end, {
-})
+-- vim.api.nvim_create_user_command("UnrealRun", function(opts)
+--     require("unreal.commands").run(opts)
+-- end, {
+-- })
 
-function setup(args)
-    print("setting up plugin")
-end
+-- vim.api.nvim_create_user_command("UnrealCD", function(opts)
+--     require("unreal.commands").SetUnrealCD(opts)
+-- end, {
+-- })
+
+-- function setup(args)
+--     print("setting up plugin")
+-- end
